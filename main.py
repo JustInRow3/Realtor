23021ce07d1f7f733327aba7e6d591245f1eff8a
Justine Rowe Jimenez <jimenezjustinerowe@gmail.com>
2023-12-16 21:22:07 +0800

    working list of locations

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
-#Chromedriver Options
-options = Options()
-options.page_load_strategy = 'eager' # Webdriver waits until DOMContentLoaded event fire is returned.
-options.headless = False #Show browser?
+# ul = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'ListItemstyles__StyledListItem-rui__zdhuws-0 fVswxu')))
+#
+url = 'https://www.realtor.com/realestateagents/'
 
-#Open tab
-wd = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-wd.implicitly_wait(10)
+from urllib.request import Request, urlopen
+req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 
-#Enter URL
-wd.get('https://www.realtor.com/realestateagents/')
-wd.maximize_window()
+web_byte = urlopen(req, timeout=10).read()
+webpage = web_byte.decode('utf-8')
+soup = BeautifulSoup(webpage, 'html.parser')
+ul = soup.findAll('li', class_="ListItemstyles__StyledListItem-rui__zdhuws-0 fVswxu")
+location_links = []
+for link in ul:
+    link_true = link.find('a', class_="base__StyledAnchor-rui__ermeke-0 eMbFNh", href=True)['href']
+    if link_true.split('/')[1] == 'realestateagents':
+        location_links.append(link_true)
+        # print(link_true)
+agent_list = r''location_links[0]
 
-#Wait
-# wait = WebDriverWait(wd, 20)  # setup wait
-# wait.until(EC.presence_of_element_located((By.ID, 'user_email'))).send_keys('*********') #Enter email
-# wait.until(EC.presence_of_element_located((By.ID, 'user_password'))).send_keys('********') #Enter Password
-# wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="sign_in_form"]/div[3]/label/div'))).click() #click checkbox
-# wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="sign_in_form"]/p[1]/input'))).click() #click Sign In
-
-time.sleep(1)
-
-#<a href="">Sign In</a>
-
+# print(ul)
+print(soup)
