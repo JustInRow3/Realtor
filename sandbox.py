b0d84b8db94396c87ee499ee8a3693fb18ae3111
Justine Rowe Jimenez <jimenezjustinerowe@gmail.com>
2024-01-08 13:54:53 +0800

    working header2

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,5 @@
-
 import src
 
-src.get_agentlist(src.url_agentlist, src.headers_fhome)
+# links = src.get_agentlist(src.url_agentlist, src.headers_fhome)
+# src.try_locationlist(links, src.headers_fagents)
+src.get_profiledetails('realestateagents/566b2816bb954c01006758f2')
--- src.py
+++ src.py
@@ -1,27 +1,113 @@
+import html
+import time
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import urllib.parse
+from requests_html import HTMLSession
+
 
 url_agentlist = 'https://www.realtor.com/realestateagents/'
+url_absolute = 'https://www.realtor.com'
 
-headers_fhome = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-           'Cache-Control': 'max-age=0',
-           'Cookie': 'split=n; split_tcv=173; __vst=26f8d4f0-7353-43fd-bcc0-ccc07731d9e0; __ssn=54ee61ed-aadb-41b4-9a07-218944269851; __ssnstarttime=1702862188; __bot=false; permutive-id=4c077a97-e0fd-4d16-9d8a-d59e9107d466; _pxvid=161bd472-9d43-11ee-87e4-b5033f3a715c; pxcts=161be382-9d43-11ee-87e4-ff04d18ffb56; AMCVS_8853394255142B6A0A4C98A4%40AdobeOrg=1; s_ecid=MCMID%7C92030571829663310514257404779208405396; AMCV_8853394255142B6A0A4C98A4%40AdobeOrg=-1124106680%7CMCIDTS%7C19710%7CMCMID%7C92030571829663310514257404779208405396%7CMCAAMLH-1703466996%7C6%7CMCAAMB-1703466996%7CRKhpRz8krg2tLO6pguXWp5olkAcUniQYPHaMWWgdJ3xzPWQmdj0y%7CMCOPTOUT-1702869397s%7CNONE%7CMCAID%7CNONE%7CvVersion%7C5.2.0; __split=8; _gcl_au=1.1.1992479585.1702862200; _px3=a9af9df9896bc47c53d623046baa2671b634127e55a77ca47afb889a5c68d3cb:M4C+UG4lUqw4IcIQI4/gcokTgNFTrtkqWmTeuvhqnxLbctYPwPlWudaaJoiYXIUNcOKVZFgFk4fnUhc5KQTYKg==:1000:K+npeJ0vhBGxyxUaFy2sxj5ePsSbi9ETg83qntm0K+1MKPN5mbIafCudbwZsJ1WyTTu5Vx4FL2yCNsUEGeLj/fNRQkbvi2Y2aQjNZBhDHUvM4ItGlICn6fsTyd90rC7qajUiGagMh8wcuCZJaJsRb6xvT/R7LOwQBGKLMWkQ5VFwvmi/DKQ6g5HndH5iMmY+SU3bUklgosMVVkDg4V6FIxjmX9/GmDxj8iU+i30OTGk=; _lr_sampling_rate=0; _rdt_uuid=1702862200938.2b27688a-c4ec-4245-ad62-f7e51c43b748; _tac=false~self|not-available; _ta=us~1~89df68bc35032d48c293adce8574aa42; _tas=sw0eto7nlv8; _scid=25bf01a3-62b0-45db-89e6-d6a303010144; _scid_r=25bf01a3-62b0-45db-89e6-d6a303010144; _lr_retry_request=true; _lr_env_src_ats=false; _ncg_sp_ses.cc72=*; _ncg_id_=87cad74b-d446-4d8e-ba7a-b591702348e9; G_ENABLED_IDPS=google; _ncg_sp_id.cc72=87cad74b-d446-4d8e-ba7a-b591702348e9.1702862204.1.1702862205.1702862204.9a8090bd-3b01-4ed2-b1f7-a77cbf1da499; _sctr=1%7C1702828800000; _tt_enable_cookie=1; _ttp=gTYXxpNUsWQQZP_qaXGatYs7JHT; _ncg_domain_id_=87cad74b-d446-4d8e-ba7a-b591702348e9.1.1702862203743.1765934203743; AMCVS_AMCV_8853394255142B6A0A4C98A4%40AdobeOrg=1; AMCV_AMCV_8853394255142B6A0A4C98A4%40AdobeOrg=-1124106680%7CMCMID%7C92030571829663310514257404779208405396%7CMCIDTS%7C19710%7CMCOPTOUT-1702869409s%7CNONE%7CvVersion%7C5.2.0; __qca=P0-353878915-1702862203159; adcloud={%22_les_v%22:%22y%2Crealtor.com%2C1702864009%22}; _ncg_g_id_=b1e85376-602d-485a-ba06-779493190c68.3.1702862208.1765934203743; ajs_anonymous_id=7c14312a-36e2-403f-ac33-9ad9f0e69a56; g_state={"i_p":1702869410361,"i_l":1}; _ga_MS5EHT6J6V=GS1.1.1702862206.1.0.1702862210.0.0.0; _ga=GA1.2.319100801.1702862198; _gid=GA1.2.611048740.1702862211; _gat=1; _uetsid=1f4242f09d4311ee88e69957a2a64e1c; _uetvid=1f423f009d4311ee8dbc0912b7840db2; mdLogger=false; kampyle_userid=c648-1aa2-6a7f-e8ae-b799-7cac-dd0e-5080; kampyleUserSession=1702862214405; kampyleUserSessionsCount=1; kampyleSessionPageCounter=1; ab.storage.sessionId.7cc9d032-9d6d-44cf-a8f5-d276489af322=%7B%22g%22%3A%221b950f8e-1579-1628-02ef-fe18635c22e9%22%2C%22e%22%3A1702864014494%2C%22c%22%3A1702862214494%2C%22l%22%3A1702862214494%7D; ab.storage.deviceId.7cc9d032-9d6d-44cf-a8f5-d276489af322=%7B%22g%22%3A%2222688168-2524-a2d4-c640-8f821afeb9f3%22%2C%22c%22%3A1702862214486%2C%22l%22%3A1702862214494%7D; ab.storage.userId.7cc9d032-9d6d-44cf-a8f5-d276489af322=%7B%22g%22%3A%22visitor_26f8d4f0-7353-43fd-bcc0-ccc07731d9e0%22%2C%22c%22%3A1702862214493%2C%22l%22%3A1702862214495%7D; cto_bundle=AVEZV19hSWs4SHRGblhYRDNid0lUMTN6RURyaWoxNGFITmtYN1E5VzFhekp0bnEybTN3UXVJRWlmMGs5MlIxSFJ0ak52Y0RTbGZ2a2FZSSUyQjhhMHJlbHQxcWhuYiUyQlBYbkJKblFBbXMzRzFIU0xrbzFRV1pDdkVlRVdIdllucGZqd2NQM08yMlpoZUxFSyUyRlRuJTJCRkpEVW9hZU1OQSUzRCUzRA; _iidt=5BOUgrG6IV/RnkOhLsHCBiQih21PESo33Fl2WA2qV2n2xm+ksUeHm28RQsYLaTiNJuZX2ErFLG+rK0qbpNfMxZ0TfXuMIXBqSQ==; _vid_t=0DOOXiDO3Y7kEL0aoQYJ+EqbddPRtB3MGtltJo86I4PAGQb1QKWblLVdcsQRcT6GKQ1SCZU4WJEGi2C024aUx0vtXbZ/nDx4YA==; __fp=zu2DWeOmVPIn4yCk6Lqs',
-           'Referer': 'https://www.realtor.com/'}
+headers_fhome = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Referer': 'https://www.realtor.com/'}
+
+headers_fagents = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Referer': 'https://www.realtor.com/realestateagents'}
+
 
 def get_agentlist(path, head):
-    location_links = [] #initialize location list
-    req = Request(path, headers=head)
-    web_byte = urlopen(req, timeout=10)
-    if web_byte.getcode() != 200:
-        return ['Error']
-    webpage = web_byte.read().decode('utf-8')
-    soup = BeautifulSoup(webpage, 'html.parser')
-    ul = soup.findAll('li', class_="ListItemstyles__StyledListItem-rui__zdhuws-0 fVswxu")
-    for link in ul:
-        link_true = link.find('a', class_="base__StyledAnchor-rui__ermeke-0 eMbFNh", href=True)['href']
-        if link_true.split('/')[1] == 'realestateagents':
-            location_links.append(link_true)
-            # print(link_true)
-    return location_links
+    session = HTMLSession()
+    location_links = []  # initialize location list
+    # req = Request(path, headers=head)
+    try:
+        r = session.get(path, headers=headers_fagents,
+                        timeout=10)
+        # web_byte = urlopen(req, timeout=10)
+        # webpage = web_byte.read().decode('utf-8')
+        soup = BeautifulSoup(r.html, 'html.parser')
+        ul = soup.findAll('li', class_="ListItemstyles__StyledListItem-rui__zdhuws-0 fVswxu")
+        for link in ul:
+            link_true = link.find('a', class_="base__StyledAnchor-rui__ermeke-0 eMbFNh", href=True)['href']
+            if link_true.split('/')[1] == 'realestateagents':
+                location_links.append(link_true)
+                # print(link_true)
+        print(f'Locations:', location_links)
+        return location_links
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
+
+
+def try_locationlist(list, head):
+    for _ in list:
+        link_ = urllib.parse.urljoin(url_absolute, _)
+        req = Request(link_, headers=head)
+        try:
+            web_byte = urlopen(req, timeout=10)
+            webpage = web_byte.read().decode('utf-8')
+            soup = BeautifulSoup(webpage, 'html.parser')
+            # Get profiles
+            profiles = soup.findAll('a', class_="jsx-3873707352")
+            location = soup.find('h2', class_="base__StyledType-rui__sc-108xfm0-0 eMHlSD")
+            hrefs_profiles = [link.get('href') for link in profiles if len(link.get('href').split('/')) == 3]
+            hrefs_profiles.insert(0, location.text)
+            set(hrefs_profiles)
+            print(f'Profiles:', hrefs_profiles)
+            time.sleep(2)
+        except urllib.error.URLError as e:
+            print(f"Error: {e}")
+
+
+def get_profiledetails(link):
+    link_ = urllib.parse.urljoin(url_absolute, link)
+    # req = Request(link_, headers=head)
+    try:
+        # web_byte = urlopen(req, timeout=10)
+        # webpage = web_byte.read().decode('utf-8')
+        session = HTMLSession()
+        r = session.get(link_, headers=headers_fagents,
+                        timeout=10)
+        r.session.close()
+        r.html.render(sleep=2, timeout=20)
+        soup = BeautifulSoup(r.html.html, 'html.parser')
+        fullname = ifexist(soup, 'h2', 'class', "base__StyledType-rui__sc-108xfm0-0 bICTqR")
+        photo = soup.find('img', class_="jsx-832586154 profile-img").get('src')
+        company = ifexist(soup, 'p', 'class', "base__StyledType-rui__sc-108xfm0-0 fgiRuk")
+        ratings = ifexist(soup, 'span', 'class', "jsx-832586154 review")
+        review = ifexist(soup, 'span', 'class', "jsx-832586154 gray-font")
+        recommended = ifexist(soup, 'span', 'class', "jsx-832586154 review pr-2")
+        section = soup.findAll(soup, 'div', 'class', 'jsx-787916864 preview-contact-details')
+        listings = ifexist(soup, 'div', 'data-testid', "component-contactDetails")
+        time.sleep(1)
+        # phone = soup.find('p', class_="jsx-787916864 d-flex")
+        # mobile = soup.find('span', class_="jsx-787916864 mobile-number")
+        # website = soup.find('a', class_="jsx-787916864 website-link")
+        # location = soup.find('p', class_="jsx-1192639173 addressspace")
+        # experience = soup.find('div', class_="jsx-1251629822 preview-profile-info preview-profile-info-right")
+        # credentials =
+        # pricerange = soup.find('div', class_="jsx-1251629822 preview-profile-info-left")
+        # areaserve = soup.find('div', class_="jsx-1418565729 preview-more-details-profile-mian")
+        # specialization = soup.find('ul', class_="jsx-1418565729 data preview-more-details-profile-li1")
+        # language = soup.find('li', class_="jsx-1418565729")
+
+    except urllib.error.URLError as e:
+        print(f"Error: {e}")
+
+
+def ifexist(soup, tag, tag_class=None, value_=None):
+    element = soup.find(tag, attrs={tag_class: value_})
+    if element:
+        return element.text
+    else:
+        return None
+
+
+# session = HTMLSession()
+# r = session.get('https://www.realtor.com/realestateagents/566b2816bb954c01006758f2', headers=headers_fagents, timeout=10)
+# time.sleep(1)
